15aa0583041a7efb5f8e1c1b243476eed0999a3d
Iwuoha Charles <iwuohacharles007@gmail.com>
2026-04-26 13:11:38 +0100

    fix production server config

--- app.js
+++ app.js
@@ -180,7 +180,12 @@ app.use((err, req, res, next) => {
 // =====================
 // SERVER
 // =====================
-const port = process.env.PORT || 3000;
-app.listen(port, () => {
-    console.log(`🚀 Server running at http://localhost:${port}`);
-});
+
+if (process.env.NODE_ENV !== 'production') {
+    const port = process.env.PORT || 3000;
+    app.listen(port, () => {
+        console.log(`🚀 Server running at http://localhost:${port}`);
+    });
+}
+
+module.exports = app;
